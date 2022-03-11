ec9642231f38c5bfd894f0c6584e3ab932d5915f
viniciusleitempergher <viniciusgabriel.com@gmail.com>
2022-03-11 14:53:30 -0300

    strings exercicio 8 feito

---
+++ 02 - Exercícios/src/strings_exercicio8/Main.java
@@ -0,0 +1,28 @@
+package strings_exercicio8;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+	public static void main(String[] args) {
+		String dataString = JOptionPane.showInputDialog("Digite uma data");
+
+		SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			Date data = df.parse(dataString);
+
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(data);
+			calendar.add(Calendar.DATE, 1);
+			
+			JOptionPane.showMessageDialog(null, df.format(calendar.getTime()));
+		} catch (ParseException e) {
+			JOptionPane.showMessageDialog(null, "Data inválida!");
+		}
+	}
+}
